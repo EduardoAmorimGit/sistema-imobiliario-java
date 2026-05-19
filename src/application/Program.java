c567fb78cf132064d62ca4b86c1cd781d99ac184
Eduardo <eduardoamorim0005@gmail.com>
2026-05-18 23:55:30 -0300

    Estudo do primeiro Objeto real

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
+import entities.Imovel;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        System.out.println("Sistema Iniciado!");
+        Imovel imovel1 = new Imovel(1, "Rua Esperança", 1850.0, "Casa",true, 75);
+
+        System.out.println(imovel1.getEndereco());
+        System.out.println(imovel1.getValor());
+        System.out.println(imovel1.isDisponivel());
 
     }
 }
