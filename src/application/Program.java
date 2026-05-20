5b42f6b5394876b2e28b0f135ab67c693e013175
Eduardo <eduardoamorim0005@gmail.com>
2026-05-19 23:36:47 -0300

    Organização de arquivos para continuidade

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,11 +5,10 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Imovel imovel1 = new Imovel(1, "Rua Esperança", 1850.0, "Casa",true, 75);
+        Imovel imovel1 = new Imovel(1, "Rua Esperança", 1850.0, "Casa", true, 75);
 
         System.out.println(imovel1.getEndereco());
         System.out.println(imovel1.getValor());
         System.out.println(imovel1.isDisponivel());
-
     }
 }
