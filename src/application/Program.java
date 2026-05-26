f08b49fcdc65480b6a3b9ac996eccf7f9eed7a69
Eduardo <eduardoamorim0005@gmail.com>
2026-05-25 23:30:37 -0300

    Adicionando a funcionalidade de criar imóveis pelo menu

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,10 +11,6 @@ public class Program {
 
         Imovel imovel1 = new Imovel(1, "Rua Esperança", 1850.0, "Casa", true, 75);
 
-        System.out.println(imovel1.getEndereco());
-        System.out.println(imovel1.getValor());
-        System.out.println(imovel1.isDisponivel());
-
         Imovel imovel2 = new Imovel(2, "Rua sacarrão", 2500.0, "Apartamento", true, 110);
 
         Imovel imovel3 = new Imovel(3, "Rua manhuaçu", 1000.0, "Casa", false, 60);
@@ -24,47 +20,6 @@ public class Program {
         listaImoveis.add(imovel1);
         listaImoveis.add(imovel2);
         listaImoveis.add(imovel3);
-
-        for (Imovel imoveis:listaImoveis) {
-            System.out.println(imoveis);
-        }
-
-            System.out.println("Insira os dados do novo imóvel");
-            System.out.println("id");
-            int id = scanner.nextInt();
-            scanner.nextLine();
-
-            System.out.println("Endereço");
-            String endereco = scanner.nextLine();
-
-            System.out.println("Valor");
-            double valor = scanner.nextDouble();
-            scanner.nextLine();
-
-            System.out.println("Tipo");
-            String tipo = scanner.nextLine();
-
-            System.out.println("Disponibilidade (Sim) ou (Não)");
-            String resposta = scanner.nextLine();
-
-            boolean disponibilidade;
-
-            if (resposta.equalsIgnoreCase("Sim")) {
-                disponibilidade = true;
-            } else {
-                disponibilidade = false;
-            }
-
-            System.out.println("Metragem");
-            int metragem = scanner.nextInt();
-
-            Imovel novoImovel = new Imovel(id, endereco, valor, tipo, disponibilidade, metragem);
-
-            listaImoveis.add(novoImovel);
-
-            for (Imovel imoveis:listaImoveis) {
-                System.out.println(imoveis);
-            }
 
         int opcao = -1;
 
@@ -82,10 +37,48 @@ public class Program {
                 case 0:
                     break;
                 case 1:
+                    System.out.println("Insira os dados do novo imóvel");
+
+                    System.out.println("id");
+                    int id = scanner.nextInt();
+                    scanner.nextLine();
+
+                    System.out.println("Endereço");
+                    String endereco = scanner.nextLine();
+
+                    System.out.println("Valor");
+                    double valor = scanner.nextDouble();
+                    scanner.nextLine();
+
+                    System.out.println("Tipo");
+                    String tipo = scanner.nextLine();
+
+                    System.out.println("Disponibilidade (Sim) ou (Não)");
+                    String resposta = scanner.nextLine();
+
+                    boolean disponibilidade;
+
+                    if (resposta.equalsIgnoreCase("Sim")) {
+                        disponibilidade = true;
+                    } else {
+                        disponibilidade = false;
+                    }
+
+                    System.out.println("Metragem");
+                    int metragem = scanner.nextInt();
+
+                    Imovel novoImovel = new Imovel(id, endereco, valor, tipo, disponibilidade, metragem);
+
+                    listaImoveis.add(novoImovel);
+                    System.out.println("Imóvel cadastrado com sucesso!");
+
                     break;
                 case 2:
                     break;
                 case 3:
+                    for (Imovel imoveis:listaImoveis) {
+                    System.out.println(imoveis);
+                }
                     break;
 
                 default:
